1f7fd7808302aac82eeb9b44de384421b5b9326d
Ashrith Channe Gowda <ChanneAM@delagelanden.com>
2023-04-25 12:07:36 +0200

    Syncfusion is woking with CDN

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,18 +2,23 @@ using EPASBlazor.Client;
 using EPASBlazor.Client.HttpRepository;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("ProductsAPI", cl => 
+builder.Services.AddHttpClient("AppAPI", cl => 
 {
     cl.BaseAddress = new Uri("https://localhost:7005/api/");
 });
 
-builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("ProductsAPI"));
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("AppAPI"));
 
 builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTgzMzQwNEAzMjMxMmUzMTJlMzMzNUdicWJOUlpOOEpQdXc0NzFXaHU1cjdLMVlUTUJFL3A5ZUJ1ckFrOFNhQlk9");
 
 await builder.Build().RunAsync();
